a99ee27322d096d5cdc09e70e80bb3ce47a09ab9
Renzo Johnson <4695400+renzojohnson@users.noreply.github.com>
2026-02-24 16:27:54 -0500

    Add file header docblocks with author info to all PHP files

--- src/Webhook/Listener.php
+++ src/Webhook/Listener.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * WhatsApp Cloud API
+ *
+ * @package   RenzoJohnson\WhatsApp
+ * @author    Renzo Johnson <hello@renzojohnson.com>
+ * @copyright 2026 Renzo Johnson
+ * @license   MIT
+ * @link      https://renzojohnson.com
+ */
+
 declare(strict_types=1);
 
 namespace RenzoJohnson\WhatsApp\Webhook;
